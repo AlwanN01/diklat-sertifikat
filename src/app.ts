fb9a7fb86e981b6339220c3a0e228558895afcc8
AlwanN01 <alwannabawi112233@gmail.com>
2022-08-18 18:27:28 +0700

    add compression

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,14 @@ import cors from 'cors'
 import morgan from 'morgan'
 import router from 'router/sertifikat'
 import path from 'path'
+import compression from 'compression'
+import helmet from 'helmet'
 import 'model/_sync'
 const app = express()
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3001' }))
+app.use(compression())
+app.use(helmet({ contentSecurityPolicy: false }))
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
